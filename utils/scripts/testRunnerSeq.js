220f5149a1f45b597f813603317b218ac67422a1
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-08-23 00:56:26 -0300

    Improvements to testRunner.js and testRunnerSeq.js

--- utils/scripts/testRunnerSeq.js
+++ utils/scripts/testRunnerSeq.js
@@ -1,19 +1,33 @@
 const { exec } = require('child_process');
 
+const fs = require('fs');
+const path = require('path');
+
 const testFolder = process.argv[2];
 
-if (!testFolder) {
-    console.error('Please provide the test folder as an argument.');
+if(!testFolder){
+    console.error('Erro: Por favor, forneça a pasta do teste como argumento.');
     process.exit(1);
 }
 
-console.log(`Executing tests in folder: ${testFolder}`);
+const testFolderPath = path.resolve(__dirname, `../../tests/${testFolder}`);
+
+if(!fs.existsSync(testFolderPath)){
+    console.error(`Erro: A pasta especificada não existe: ${testFolderPath}`);
+    process.exit(1);
+}
+
+console.log(`Executando testes na pasta: ${testFolderPath}`);
 
 exec(`cross-env JEST_PUPPETEER_HEADLESS=false jest --runInBand --detectOpenHandles ./tests/${testFolder}`, (err, stdout, stderr) => {
-    if (err) {
-        console.error(`Error executing test: ${err}`);
+    if(err){
+        console.error(`Erro ao executar o teste: ${err.message}`);
         return;
     }
+
     console.log(stdout);
-    console.error(stderr);
+    
+    if(stderr){
+        console.error(`Erros durante a execução do teste: ${stderr}`);
+    }
 });
